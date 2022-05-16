36a410fdb916558987e9deb7a6051b45e87df03b
deepak <dy195360@gmail.com>
2022-05-16 12:42:31 +0530

    updated

--- script.js
+++ script.js
@@ -1,22 +1,67 @@
-let txt = document.getElementById('ip').value;
+let add = document.querySelector('.add');
 let rmv = document.querySelector('.inp');
-let el1 = document.querySelector('.list')
-let obj ={
-    dis:txt,
-    id:0,
-    compl:false,
-    del:false
-}
-function addlist()
+let el = document.querySelector('.list');
+let inpt = document.querySelector('#ip');
+let el2 = document.querySelector('.disc');
+let todoarr = [],i=0;
+function addtodo(txt)
 {
- let txt1 = `
-     <div class="check">
-         <img src="./image/oval.png" alt="circle" height="100%" width="100%">
-     </div>
-     <div class="disc">${txt}</div>
-     <div class="del"><img src="./image/bin.png" alt="del" height="100%" width="100%"></div>
- `
-el1.appendChild('li');
-
+    if(txt!=""){
+  let arr={
+      text:txt,
+      id:i++,
+      del:false,
+      compl:false
+  }
+  todoarr.push(arr);
+  createtodo();
 }
-rmv.addEventListener('click',addlist());
+inpt.value=''
+}
+
+function createtodo(){
+    el.innerHTML='';
+    todoarr.forEach(function(todo)
+    {    if(todo.del===false)
+        {
+            let lin = todo.compl?"line":null;
+        let li = document.createElement('li');
+        let temp = `<div class="check">
+        <img src="./image/oval.png" alt="circle" height="100%" width="100%" class="img" id=${todo.id}>
+    </div>
+    <div class="disc">${todo.text}</div>
+    <div class="del"><img src="./image/bin.png" alt="del" height="100%" width="100%" class = "det" id=${todo.id}></div>`
+    li.innerHTML=temp;
+    //console.log(li);
+    el.appendChild(li);
+        }
+    })
+}
+
+add.addEventListener('click',function(){
+    addtodo(inpt.value);
+})
+el.addEventListener('click',function(ev){
+    //console.log('lust clicked');
+ if(ev.target.classList.contains('img'))
+ {console.log('img clicked');
+    let tid=ev.target.getAttribute('id');
+    //console.log(tid);
+    todoarr.forEach(function(todo){
+        if(todo.id == tid)
+         todo.compl=!todo.compl;
+    })
+    createtodo();
+ }
+ if(ev.target.classList.contains('det'))
+ {console.log('del clicked');
+    let tdel=ev.target.getAttribute('id');
+    //console.log(todoarr);
+    todoarr.forEach(function(todo){
+        if(todo.id == tdel)
+         todo.del= true;
+    })
+    createtodo();
+ }
+}
+)
